cdfa0d0c003fb308945de8b96ec04ae31c80df5d
imantaba <itn.taba@gmail.com>
2022-10-18 22:31:09 +0330

    palindrome completed

--- palindrom.py
+++ palindrom.py
@@ -4,18 +4,43 @@
 #     adbc -> False
 # ````
 
-def isPalindromeWithOneCharRemove(string):
-    if not string:
-        return True
-    if len(string) <= 3:
-        return True
-    pal_char_count = int(len(string)/2-1)
-    pchc_end = pal_char_count + 1
-    print(pchc_end)
-    for char in string:
-        new_str = string.replace(char, '')
-        print(new_str[pchc_end:])
-        if new_str[:pal_char_count] == new_str[pchc_end::-1]:
-            print("palindrom")
+# def isPalindromeWithOneCharRemove(string):
+#     if not string:
+#         return True
+#     if len(string) <= 3:
+#         return True
+#     pal_char_count = int(len(string)/2-1)
+#     pchc_end = pal_char_count + 1
+#     for char in string:
+#         new_str = string.replace(char, '')
+#         if new_str[:pal_char_count] == new_str[:pal_char_count:-1]:
+#             return True
 
-isPalindromeWithOneCharRemove('tacocats')
+# isPalindromeWithOneCharRemove('adbc')
+
+class IsPalindrome:
+    def __init__(self, string):
+        self._generate = _Calculate(string)
+    def showResult(self):
+        result = self._generate.isPalindromeWithOneCharRemove()
+        print(result)
+        
+class _Calculate:
+    def __init__(self, string):
+        self._str = string
+    def isPalindromeWithOneCharRemove(self):
+        string = self._str
+        if not string:
+            return True
+        if len(string) <= 3:
+            return True
+        pal_char_count = int(len(string)/2-1)
+        pchc_end = pal_char_count + 1
+        for char in string:
+            new_str = string.replace(char, '')
+            if new_str[:pal_char_count] == new_str[:pal_char_count:-1]:
+                return True  
+
+s = "racercar"
+a = IsPalindrome(s)
+a.showResult()
