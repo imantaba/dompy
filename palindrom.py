c12ab405469402560ebc2368cf1a66d055c63c47
imantaba <itn.taba@gmail.com>
2022-10-18 22:32:08 +0330

    palindrome remove extra lines

--- palindrom.py
+++ palindrom.py
@@ -4,19 +4,6 @@
 #     adbc -> False
 # ````
 
-# def isPalindromeWithOneCharRemove(string):
-#     if not string:
-#         return True
-#     if len(string) <= 3:
-#         return True
-#     pal_char_count = int(len(string)/2-1)
-#     pchc_end = pal_char_count + 1
-#     for char in string:
-#         new_str = string.replace(char, '')
-#         if new_str[:pal_char_count] == new_str[:pal_char_count:-1]:
-#             return True
-
-# isPalindromeWithOneCharRemove('adbc')
 
 class IsPalindrome:
     def __init__(self, string):
