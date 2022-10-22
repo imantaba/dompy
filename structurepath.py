0ffbe2e3507b8273cf56f9a6333cff0ec8636b58
imantaba <itn.taba@gmail.com>
2022-10-22 14:58:50 +0330

    add init file

---
+++ structurepath.py
@@ -0,0 +1,22 @@
+files = [
+    "/wehabpp/js/a.html",
+    "/wehabpp/js/b.html",
+    "/wehabpp/im.js",
+    "/wehabpp/fl/my.py",
+    "/wehabpp/js.html"
+]
+
+# wehabpp
+#   --js
+#     --a.html
+#     --b.html
+#   --im.js
+
+# def parse_by_slash(paths):
+#     for path in paths:
+#         pice = path.split('/')
+#         it = []
+#         for item in pice:
+#             if item not in it:
+#                 print(item)
+# parse_by_slash(files)
