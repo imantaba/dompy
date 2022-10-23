b19381ef482d23ee7ec81ae62d64d5fbdcd41906
imantaba <itn.taba@gmail.com>
2022-10-23 22:56:16 +0330

    add num of alive

---
+++ num_of_alive.py
@@ -0,0 +1,8 @@
+
+# Given an array of birth and death years, return a year that has the highest number of alive people.
+# Assume no one dies before they're one aka death year > birth year
+# Ex :
+# [[2000,2005],[1990,2005],[2005,2010],[1980,1995]]   return -> 2005
+
+list = [[2000,2005],[1990,2005],[2005,2010],[1980,1995]]
+
