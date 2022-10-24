50beb9fdf949a05776de24d2fb570cbd305012b0
imantaba <itn.taba@gmail.com>
2022-10-24 22:51:37 +0330

    fix year with max alive

--- year_with_max_alive.py
+++ year_with_max_alive.py
@@ -5,9 +5,17 @@
 # [[2000,2005],[1990,2005],[2005,2010],[1980,1995]]   return -> 2005
 
 list = [[2000,2005],[1990,2005],[2005,2010],[1980,1995]]
-
+dic = {}
+complete_list = []
 for items in list:
-    for i in range(items[0], items[1]):
-        print(i)
-    print("##################")
-        
+    for i in range(items[0], items[1]+1):
+        complete_list.append(i)
+
+for year in complete_list:
+    if year in dic:
+        dic[year] += 1
+    else:
+        dic[year] = 1
+
+value_list = []
+print(max(dic, key=dic.get))   
