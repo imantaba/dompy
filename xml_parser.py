f5062f3e03ee50972e7f377b7e0f52a897b6261e
imantaba <itn.taba@gmail.com>
2022-12-03 11:08:11 +0330

    washing

--- xml_parser.py
+++ xml_parser.py
@@ -1,6 +1,17 @@
-# <A>
-# <B>
-# <C> Y </C>
-# </B>
-# <D> U </D>
-# </A>
+from xml.dom.minidom import parse, parseString, Node
+
+
+def set_id_attribute(parent, attribute_name="id"):
+    if parent.nodeType == Node.ELEMENT_NODE:
+        if parent.hasAttribute(attribute_name):
+            parent.setIdAttribute(attribute_name)
+    for child in parent.childNodes:
+        print(child)
+        set_id_attribute(child, attribute_name)
+        # print(f"'nodeType:'{parent.nodeType} 'element node:' {Node.ELEMENT_NODE}")
+
+
+
+document = parse("smiley.svg")
+set_id_attribute(document)
+print(document.getElementById("skin"))
