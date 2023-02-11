6edc753424f6ffdb3f7e7a76049b00def450b84d
imantaba <itn.taba@gmail.com>
2023-02-11 22:43:44 +0330

    openai

---
+++ openai-autocompletion.py
@@ -0,0 +1,16 @@
+
+# validate an email with regex and comment each line 
+
+import re  # import regex library
+
+# define regex pattern for email validation
+pattern = r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$"
+
+# get the email from user input
+email = input("Please enter your email: ")
+
+# validate the email using regex pattern 
+if re.match(pattern, email):  # if pattern matches with the given string 
+    print("Valid Email")  # print valid message 
+else:   # else print invalid message 
+    print("Invalid Email")
